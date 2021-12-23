782efe9f746c85cbb753ee365b5ffac2c3455cff
jachs <799516423@qq.com>
2021-12-23 10:52:38 +0800

    ASE

---
+++ boot/j-batch/src/main/java/com/jachs/jbatch/entity/Book.java
@@ -0,0 +1,19 @@
+package com.jachs.jbatch.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/***
+ * 
+ * @author zhanchaohan
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+	private String name;
+	private double price;
+	
+}
